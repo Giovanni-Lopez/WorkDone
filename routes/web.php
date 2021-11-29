f4c64f996436044dd6955a82377320be2ae8a47d
Giovanni-Lopez <alexisange001@gmail.com>
2021-11-29 00:56:34 -0600

    Se crearon las rutas para poder acceder al login y register, al ser llamadas desde los correspondientes controladores.

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\loginController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,8 +19,17 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::get('/', HomeController::class);
+Route::get('/', function () {
+    return view('home');
+});
 
-Route::get('login', [loginController::class, 'iniciarSesion'] );
+Route::get('/login', [SessionsController::class, 'iniciarSesion'])->name('login.iniciarSesion');
+
+Route::get('/register', [RegisterController::class, 'create'])->name('register.create');
+
+/*   Route::get('/', HomeController::class); */
+
+/* Route::get('login', [loginController::class, 'iniciarSesion'] ); */
 
 /* Route::get('users',[ UserController::class, '']); */
+

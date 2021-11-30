95d33dbd9fa1dc800767b6bd860db5bf8c3c4ce2
Giovanni-Lopez <alexisange001@gmail.com>
2021-11-30 14:04:27 -0600

    Se agrego la ruta account.

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\loginController;
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
+use App\Http\Controllers\AccountController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,13 +18,30 @@ use App\Http\Controllers\SessionsController;
 
 Route::get('/', function () {
     return view('home');
-});
+})->middleware('auth');
 
-Route::get('/login', [SessionsController::class, 'iniciarSesion'])->name('login.iniciarSesion');
+Route::get('/login', [SessionsController::class, 'iniciarSesion'])
+    ->middleware('guest')
+    ->name('login.iniciarSesion');
 
-Route::get('/register', [RegisterController::class, 'create'])->name('register.create');
+Route::post('/login', [SessionsController::class, 'store'])
+    ->name('login.store');
 
-Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+//register
+Route::get('/register', [RegisterController::class, 'create'])
+    ->middleware('guest')
+    ->name('register.create');
+
+Route::post('/register', [RegisterController::class, 'store'])
+    ->name('register.store');
+
+Route::get('/account', [AccountController::class, 'activate'])
+    ->middleware('guest')
+    ->name('account.activate'); 
+
+Route::get('/logout', [SessionsController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('logout.destroy');
 
 /*   Route::get('/', HomeController::class); */
 

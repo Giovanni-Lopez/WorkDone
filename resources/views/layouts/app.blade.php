ad288f16463af573f03a8dccffdec2ed22c9800d
Giovanni-Lopez <alexisange001@gmail.com>
2021-11-30 14:16:00 -0600

    Se agrego una estructura selectiva doble, para cuando el usuario se haya logiado, se le habilite el nombre y enlace para cerrar session.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,18 +15,39 @@
 
     <nav class="flex py-5 bg-indigo-500 text-white">
         <div class="w-1/2 px-12 mr-auto">
-            <p class="text-2x1 font-bold">My Aplication</p>
+            <p class="text-2x1 font-bold">WorkDone</p>
 
         </div>
 
         <ul class="w-1/2 px-16 ml-auto flex justify-end pt-1">
-            <li class="mx-4">
-                <a href="{{route('login.iniciarSesion')}} " class="font-semibold hover:bg-indigo-700 py-3 px-4 rounded-md">Log In</a>
-            </li>
 
-            <li>
-                <a href=" {{route('register.create')}} " class="font-semibold border-2 border-white py-2 px-4 rounded-md hover:bg-white hover:text-indigo-700">Register</a>
-            </li>
+          @if (auth()->check())
+
+              <li class="mx-8">
+                  <p class="text-x1">Welcome <b> {{ auth()->user()->first_name }} </b></p>
+              </li>
+       
+              <li>
+                <a href=" {{route('logout.destroy')}} " class="font-bold 
+                border-2 border-white py-2 px-4 rounded-md bg-red-500 hover:bg-red-600">Log Out</a>
+              </li>
+
+          @else
+          <li class="mx-6">
+            <a href="{{route('login.iniciarSesion')}} " class="font-semibold 
+            hover:bg-indigo-700 py-3 px-4 rounded-md">Log In</a>
+          </li>
+
+          <li>
+              <a href=" {{route('register.create')}} " class="font-semibold 
+              border-2 border-white py-2 px-4 rounded-md hover:bg-white hover:text-indigo-700">Register</a>
+          </li>
+
+          @endif
+
+            
+
+
         </ul>
 
     </nav>

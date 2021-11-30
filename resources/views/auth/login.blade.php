51737ce7ee61a5b86967f1e10eb3d2bab3452ff7
Giovanni-Lopez <alexisange001@gmail.com>
2021-11-30 14:12:38 -0600

    se agrego un mensaje que al fallar la validacion se muestre al usuario..

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
 
     <form action="" class="mt-4" method="post">
 
-        <img src=" {{ asset('images/logo.png') }} ">
+        <img src=" {{ asset('images/logo.png') }} "><br>
 
         @csrf
         
@@ -17,13 +17,16 @@
         text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="Email">
 
         <input type="password" name="password" id="password" class="border border-gray-200 rounded-md bg-gray-200 w-full
-        text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="Password">
+        text-lg placeholder-gray-900 p-2 my-7 focus:bg-white" placeholder="Password">
 
-        <p class="border border-red-500 rounded-md bg-red-100 w-full 
-        text-red-600 p-2 my-2">* Error</p>
+        @error('message')
+            <p class="border border-red-500 rounded-md bg-red-100 w-full 
+            text-red-600 p-2 my-2"> {{ $message }} </p>
+        @enderror
+        
 
         <button type="submit" class="rounded-md bg-red-500 w-full text-lg text-white font-semibold p-2 
-        my-3 hover:bg-indigo-600">Sign In</button>
+        my-6 hover:bg-indigo-600">Sign In</button>
 
     </form>
 

b2fbd90821e3dd9a3acbb5889e516eebe42d01cc
Giovanni-Lopez <alexisange001@gmail.com>
2021-11-29 19:52:58 -0600

    Se hicieron modificaciones, se agrego una imagen de logo en el formulario, se agregaron mas input, se cambio el texto del boton.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,26 +4,40 @@
     
 @section('content')
     
-<div class="block mx-auto my-12 p-8 bg-white w-1/3 border border-gray-200 rounded-lg shadow-lg">
-
-    <h1  class="text-pink-900 text-3x1 text-center font-bold" >Register</h1>
+<div class="block mx-auto my-12 p-8 bg-white w-1/3 border border-gray-200 rounded-lg shadow-lg">    
 
     <form action="" class="mt-4" method="post">
+        @csrf
 
-        <input type="text" name="name" id="name" class="border border-gray-200 rounded-md bg-gray-200 w-full
-        text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="Name">
+        <img src=" {{ asset('images/logo.png') }} ">
 
+        <label>First name
+        <input type="text" name="first_name" id="first_name" class="border border-gray-200 rounded-md bg-gray-200 w-full
+        text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="">
+        </label>
+
+        <label>Last name            
+            <input type="text" name="last_name" id="last_name" class="border border-gray-200 rounded-md bg-gray-200 w-full
+            text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="">
+        </label>        
+
+        <label>Email
         <input type="email" name="email" id="email" class="border border-gray-200 rounded-md bg-gray-200 w-full
-        text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="Email">
+        text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="">
+        </label>
 
+        <label>Password
         <input type="password" name="password" id="password" class="border border-gray-200 rounded-md bg-gray-200 w-full
-        text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="Password">
+        text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="">
+        </label>
 
+        <label>Confirm password
         <input type="password" name="password_confirmation" id="password_confirmation" class="border border-gray-200 rounded-md bg-gray-200 w-full
-        text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="Password Confirmation">
+        text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="">
+        </label>
 
-        <button type="submit" class="rounded-md bg-indigo-500 w-full text-lg text-white font-semibold p-2 
-        my-3 hover:bg-indigo-600">Send</button>
+        <button type="submit"  class="rounded-md bg-red-500 w-full text-lg text-white font-semibold p-2 
+        my-3 hover:bg-indigo-600">Sign Up</button>
 
     </form>
 

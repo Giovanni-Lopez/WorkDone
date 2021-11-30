ca714ff1cd6e14cf1b9d897893cb4764de252272
Giovanni-Lopez <alexisange001@gmail.com>
2021-11-29 19:51:24 -0600

    Se hicieron modificaciones, se agrego el logo y te sambio el nombre del boton que se muestra al usuario.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,11 +5,14 @@
 @section('content')
 
 <div class="block mx-auto my-12 p-8 bg-white w-1/3 border border-gray-200 rounded-lg shadow-lg">
-
-    <h1  class="text-pink-900 text-3x1 text-center font-bold" >Login</h1>
+   
 
     <form action="" class="mt-4" method="post">
 
+        <img src=" {{ asset('images/logo.png') }} ">
+
+        @csrf
+        
         <input type="email" name="email" id="email" class="border border-gray-200 rounded-md bg-gray-200 w-full
         text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="Email">
 
@@ -19,8 +22,8 @@
         <p class="border border-red-500 rounded-md bg-red-100 w-full 
         text-red-600 p-2 my-2">* Error</p>
 
-        <button type="submit" class="rounded-md bg-indigo-500 w-full text-lg text-white font-semibold p-2 
-        my-3 hover:bg-indigo-600">Send</button>
+        <button type="submit" class="rounded-md bg-red-500 w-full text-lg text-white font-semibold p-2 
+        my-3 hover:bg-indigo-600">Sign In</button>
 
     </form>
 

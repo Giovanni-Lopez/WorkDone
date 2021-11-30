82cbb6e11bbcdf29e22e3498c68fd869d2253608
Giovanni-Lopez <alexisange001@gmail.com>
2021-11-29 19:47:04 -0600

    se agrego el metodo store para que este pueda guardar los usuarios, y retornar la vista principal.

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use PhpParser\Node\Expr\FuncCall;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
@@ -10,5 +12,11 @@ class RegisterController extends Controller
 
     public function create(){
         return view('auth.register');
+    }
+
+    public function store(){
+        $user = User::create(request( ['first_name', 'last_name', 'email', 'password'] ));
+        auth()->login($user);
+        return redirect()->to('/');
     }
 }

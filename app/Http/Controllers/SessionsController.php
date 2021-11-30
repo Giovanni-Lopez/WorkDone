bee25bfea781a171ad00b00e04eae0bf9e9b4e27
Giovanni-Lopez <alexisange001@gmail.com>
2021-11-30 14:08:20 -0600

    Se creo el metodo store, ademas de eso se agrego una estructura selectiva simple, para aplica la respectiva validacion de cada campo requerido.

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,11 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Auth\Events\Logout;
 
 class SessionsController extends Controller
 {
     //
     public function iniciarSesion(){
         return view('auth.login');
+    }
+
+    public function store(){
+        if(auth()->attempt(request(['email', 'password'])) == false){
+            return back()->withErrors([
+                'message' => 'The email or passwork is incorrect, please try again'
+            ]);
+        }
+
+        return redirect()->to('/'); 
+    }
+
+    public function destroy(){
+        auth()->logout();
+
+        return redirect()->to('/');
     }
 }

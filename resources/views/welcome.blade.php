0c0e115cb792af237aa2fcfb47c46e0220793f6c
Giovanni-Lopez <alexisange001@gmail.com>
2021-12-02 00:24:15 -0600

    se borro codigo inutilizable.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <title>Document</title>
+    <title>El Salvador</title>
 </head>
 <body>
-    <h1>hola 1</h1>
-    <h2>hola 2</h2>
-    <h3>Hola 3</h3>
-    <h4>hola 4</h4>
-    <h5>hola 5</h5>
-    <h6>hola 6</h6>
+    <h1>prueba 1</h1>    
 </body>
 </html>

9ae8b1b97225a8f6c5e35a2167de7da0b6e5ec00
Giovanni-Lopez <alexisange001@gmail.com>
2021-12-02 00:39:42 -0600

    Se añadieron las credenciales que debe de usar el rol de admin.
    Se debe de usar primero este comando:  php artisan migrate:fresh
    
    Luego de haber migrado todas las migraciones:  php artisan db:seed, para que se genere el rol de admin.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use GuzzleHttp\Psr7\Request;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-    }
+        
+        $usu = new User();
+
+        $usu->first_name = 'Admin';
+        $usu->last_name = 'Admin';
+        $usu->email = 'Admin@gmail.com';
+        $usu->password = '1234';
+        $usu->role = 'admin';
+
+        $usu->save();
+
+    } 
 }
